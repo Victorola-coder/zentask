d8c1c4a520f309426d807f1194ab5062a7d64ac2
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2025-11-25 09:26:05 +0100

    refactor: remove authentication, profile, API, caching, and AIO component modules.

--- app/components/global/index.ts
+++ app/components/global/index.ts
@@ -1,5 +1,4 @@
 export { default as AOS } from "./aos";
-export { default as AIO } from "./aio";
 export { default as Glow } from "./glow";
 export { default as Loader } from "./loader";
 export { default as Animation } from "./animation";
